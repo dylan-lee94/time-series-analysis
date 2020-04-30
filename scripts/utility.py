c86b317f38f5cad289f6f718a491c6a819b1da8a
Dylan Lee <46657030+dylan-lee94@users.noreply.github.com>
2020-04-30 16:36:19 +0200

    Update utility.py

--- scripts/utility.py
+++ scripts/utility.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math as m
+import os
+import pandas as pd
 
 def histnorm(x,n_bins=20):
     count, bin_edges = np.histogram(x,n_bins)
@@ -8,11 +10,21 @@ def histnorm(x,n_bins=20):
     f = count/(count*widths).sum()
     return (f,bin_centers)
 
-
-
+# Parametric VaR and CVaR
 def VAR(ts,alpha=0.05):
     return -np.quantile(ts,alpha)
 
 def CVAR(ts,alpha=0.05):
     k = m.ceil(len(ts)*alpha) # Ceil due to Zero-based numbering
     return -np.mean(np.sort(ts)[:k]) 
+
+# Load Data
+def load_data(name):
+    os.chdir("../data")
+    path = os.path.join(os.getcwd(),"dataset",'DASH_merged.txt')
+    df = pd.read_csv(path, sep = ',')
+    df.drop(labels='Unnamed: 0',axis=1,inplace=True)
+    df.time = pd.to_datetime(df.time,unit='s')
+    df.set_index('time',inplace=True)
+    return df
+
