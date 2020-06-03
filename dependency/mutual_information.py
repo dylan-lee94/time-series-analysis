ff018e3a2d7851f09de6b59d3ea747a843f69738
Dylan Lee <46657030+dylan-lee94@users.noreply.github.com>
2020-06-03 13:24:15 +0200

    Update mutual_information.py

--- dependency/mutual_information.py
+++ dependency/mutual_information.py
@@ -5,7 +5,7 @@ from entropy import entropy, joint_entropy
 # np.where() evalutes before condition
 np.seterr(divide='ignore',invalid='ignore')
 
-#%% Calculate the mutual information in terms of entropy
+#%% Calculate the Mutual Information in terms of entropy
 
 def mutual_information(x,y,nbins=20):
     Hx = entropy(x,nbins)
@@ -15,5 +15,22 @@ def mutual_information(x,y,nbins=20):
     NMI = 2*MI/(Hx + Hy)
     return MI, NMI
 
-# Example: 
-# MI,NMI = mutual_information(x,y)
+#%% Calculate the Conditional Mutual Information in terms of entropy
+# x independent variable
+# y dependent variable (lagged)
+# z is lagged x
+
+def cond_mutual_information(x,y,z,nbins=20):
+    Hxz = joint_entropy(x,z,nbins)
+    Hyz = joint_entropy(y,z,nbins)
+    Hz = entropy(z,nbins)
+
+    # Mutual Information for 3 Variables
+    count_xyz,edges = np.histogramdd(np.array([x,y,z]).T, bins=20)
+    p_xyz = count_xyz/len(x)
+    Hxyz = -np.sum(np.sum(np.sum(np.where(p_xyz>0, np.log2(p_xyz)*p_xyz,0))))
+    
+    # Conditional Mutual Information
+    MIxy_z =  Hxz + Hyz - Hxyz - Hz
+    NMIxy_z = 2*MIxy_z/(Hxz + Hyz)
+    return MIxy_z, NMIxy_z
